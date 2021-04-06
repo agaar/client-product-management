d75a4436d0e6b78ba12d3da2fd36498418d1abb4
agar91 <agnieszkarapa@gmail.com>
2021-04-06 15:46:51 +0200

    admin components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { RegisterComponent } from './components/user/register/register.component
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { HomeComponent } from './components/user/home/home.component';
 import { DetailsComponent } from './components/user/details/details.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { UserListComponent } from './components/admin/user-list/user-list.component';
+import { ProductListComponent } from './components/admin/product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { DetailsComponent } from './components/user/details/details.component';
     RegisterComponent,
     ProfileComponent,
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
+    DashboardComponent,
+    UserListComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
