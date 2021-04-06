28dcf0e68944c22c228512d16990178aec340a81
agar91 <agnieszkarapa@gmail.com>
2021-04-06 15:44:17 +0200

    user components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { HomeComponent } from './components/user/home/home.component';
+import { DetailsComponent } from './components/user/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
