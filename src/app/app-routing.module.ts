2acfc4c424e9c9d1f6f9a68a1ee6762d13e8992f
agar91 <agnieszkarapa@gmail.com>
2021-04-09 14:16:53 +0200

    routers added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/user/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { DetailsComponent } from './components/user/details/details.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { UserListComponent } from './components/admin/user-list/user-list.component';
+import { ProductListComponent } from './components/admin/product-list/product-list.component';
+import { NotFoundComponent } from './components/error/not-found/not-found.component';
+import { UnauthorizedComponent } from './components/error/unauthorized/unauthorized.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //main page
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'detail', component: DetailsComponent},
+  {path: 'detail/:id', component: DetailsComponent},
+
+  //admin panel
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'user-list', component: UserListComponent},
+  {path: 'product-list', component: ProductListComponent},
+
+  //error pages
+  {path: '404', component: NotFoundComponent},
+  {path: '401', component: UnauthorizedComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
