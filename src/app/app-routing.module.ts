3e22128cf9e390110449633ed1359e309ac52321
agar91 <agnieszkarapa@gmail.com>
2021-04-13 12:34:54 +0200

    Guard Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/user/home/home.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { RegisterComponent } from './components/user/register/register.component';
@@ -10,21 +10,36 @@ import { UserListComponent } from './components/admin/user-list/user-list.compon
 import { ProductListComponent } from './components/admin/product-list/product-list.component';
 import { NotFoundComponent } from './components/error/not-found/not-found.component';
 import { UnauthorizedComponent } from './components/error/unauthorized/unauthorized.component';
-
+import { AuthGuard} from './guards/auth.guard';
+import { Role} from './model/role';
+ 
 const routes: Routes = [
   //main page
   {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
+  //user page
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent},
+  {path: 'profile', component: ProfileComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN, Role.USER]}
+},
   {path: 'detail', component: DetailsComponent},
   {path: 'detail/:id', component: DetailsComponent},
 
   //admin panel
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'user-list', component: UserListComponent},
-  {path: 'product-list', component: ProductListComponent},
+  {path: 'dashboard', component: DashboardComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN]}
+},
+  {path: 'user-list', component: UserListComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN]}
+},
+  {path: 'product-list', component: ProductListComponent,
+  canActivate: [AuthGuard],
+  data: {roles: [Role.ADMIN]}
+},
 
   //error pages
   {path: '404', component: NotFoundComponent},
@@ -36,4 +51,11 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule { 
+  constructor(private router:Router){
+    this.router.errorHandler = (error: any) => {
+      this.router.navigate(['/404']);
+    }
+  }
+}
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  
+  constructor(private router: Router,
+    private userService: UserService){
+
+    }
+  
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    const currentUser = this.userService.currentUserValue;
+    if(currentUser){
+      //check if route is restricted by role ...
+      if(route.data.roles && route.data.roles.indexOf(currentUser.role)===-1){
+        this.router.navigate(['/401']);
+        return false;
+      }
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
   }
   
-}
+  
+ 
+
+
+
+  // : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   // return true;
+  }
+  
+//}
