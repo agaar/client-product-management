4faa5073809256b340fa1e0287158e1496c2b8bd
agar91 <agnieszkarapa@gmail.com>
2021-04-13 13:29:02 +0200

    Register component

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
-  constructor() { }
+export class RegisterComponent implements OnInit {
+  user:User = new User();
+  errorMessage: string;
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  register(){
+    this.userService.register(this.user).subscribe((data: any) => {
+      this.router.navigate(['/login']);
+    },(err: any)=>{
+      this.errorMessage = "Username already exists"
+    })
   }
 
 }
