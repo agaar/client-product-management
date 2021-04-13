abc5f408f2eb3ff9dbeacf2c3255003c2378d276
agar91 <agnieszkarapa@gmail.com>
2021-04-13 15:45:24 +0200

    User Template

--- src/app/components/template/user-template/user-template.component.ts
+++ src/app/components/template/user-template/user-template.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/model/role';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-template',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-template.component.css']
 })
 export class UserTemplateComponent implements OnInit {
-
-  constructor() { }
+  currentUser: User;
+  constructor(private userService: UserService) { 
+    this.currentUser = this.userService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
 
   ngOnInit(): void {
   }
 
-}
+  logOut(){
+    this.userService.logOut().subscribe(data => {
+      this.router.navigate(['/login']);
+    });
+  }
+
+    get isAdmin(){
+      return this.currentUser && this.currentUser.role === Role.ADMIN;
+    }
+ }
