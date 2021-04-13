4c901731377f6e99ab03d4e71889079595d3a588
agar91 <agnieszkarapa@gmail.com>
2021-04-13 15:10:53 +0200

    Product Component

--- src/app/components/user/details/details.component.ts
+++ src/app/components/user/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  productId: string;
+  currentProduct: Product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.currentProduct = JSON.parse(localStorage.getItem("currentProduct") || '{}');
+  }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      if(params.has('id')){
+        this.productId = params.get('id');
+      }
+    });
   }
 
 }
