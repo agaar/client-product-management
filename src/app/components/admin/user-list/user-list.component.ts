ef1851c3a7389cc0ac17d6a10c06b5c7ef300dd6
agar91 <agnieszkarapa@gmail.com>
2021-04-14 12:06:12 +0200

    UserList component

--- src/app/components/admin/user-list/user-list.component.ts
+++ src/app/components/admin/user-list/user-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
+import {MatPaginator, MatTableDataSource, MatSort} from "@angular/material";
+import { User } from 'src/app/model/user';
+import { $ } from 'protractor';
+
+declare var $any;
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +12,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  userList: Array<User>;
+  dataSource: MatTableDataSource<User> = new MatTableDataSource();
+  displayedColumns: string[] = ['id' , 'name', 'username' , 'action'];
+  selectedUser: User = new User();
+  errorMessage: string;
+  infoMessage: string;
 
-  constructor() { }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.findAllUsers();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  findAllUsers(){
+    this.adminService.findAllUsers().subscribe(data => {
+      this.userList = data;
+      this.dataSource.data = data;
+    });
+  }
+
+  editUserRequest(user: User) {
+    this.selectedUser = user;
+    $("#userModal").modal('show');
+  }
+
+  editUser(){
+    this.adminService.updateUser(this.selectedUser).subscribe(data => {
+      let itemIndex = this.userList.findIndex(item=>item.id == this.selectedUser.id);
+      this.userList[itemIndex] = this.selectedUser;
+      this.dataSource = new MatTableDataSource(this.userList);
+      this.infoMessage = "Mission is completed";
+      $("#userModal").modal('hide');
+    },err => {
+      this.errorMessage = "Unexpected error occurred.";
+    });
+  }
+
+  deleteUserRequest(user: User) {
+    this.selectedUser = user;
+    $("#deleteModal").modal('show');
+  }
+
+  deleteUser(){
+    this.adminService.deleteUser(this.selectedUser).subscribe(data => {
+      let itemIndex = this.userList.findIndex(item=>item.id == this.selectedUser.id);
+      if(itemIndex !== -1){
+        this.userList.splice(itemIndex, 1);
+      }
+      this.dataSource = new MatTableDataSource(this.userList);
+      this.infoMessage = "Mission is completed";
+      $("#deleteModal").modal('hide');
+    },err => {
+      this.errorMessage = "Unexpected error occurred.";
+    });
   }
 
 }
