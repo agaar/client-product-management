a5982e954a0b209949d51156a6f257d20f0004b0
agar91 <agnieszkarapa@gmail.com>
2021-04-19 13:31:36 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatIconModule } from '@angular/material/Icon';
+import { MatIconModule } from '@angular/material/icon';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
